ef8f26dee9cc70131b6b62a1a629d9e352ea82ef
Andres Vega <aevega1991@gmail.com>
2021-06-03 12:44:18 -0300

    Variables y constantes

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,11 @@
+const nombre = 'Fernando';
+let apellido = 'Vega';
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre,apellido,valorDado);
+if (true){
+    console.log("se reescribe la variable valorDado");
+    let valorDado = 6;
+    console.log(valorDado);
+}
