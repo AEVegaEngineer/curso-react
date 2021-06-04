5d3188104355b1ea75831c1378a27d02cb5a72f3
Andres Vega <aevega1991@gmail.com>
2021-06-04 11:23:11 -0300

    22 Promesas

--- 02-intro-javascript/src/bases/09-Multiples-exportaciones-por-defecto.js
+++ 02-intro-javascript/src/bases/09-Multiples-exportaciones-por-defecto.js
@@ -1,14 +1,19 @@
-import heroes, {owners} from './data/heroes';
+import heroes/*, {owners}*/ from '../data/heroes';
 
-console.log(owners);
+//console.log(owners);
 
 const getHeroeById = (id) => {
     return heroes.find((heroe) => heroe.id === id)
 }
-console.log(getHeroeById(2));
+//console.log(getHeroeById(2));
 
 //tarea
 const getHeroesByOwner = (owner) => {
     return heroes.filter((heroe) => heroe.owner === owner)
 }
-console.log(getHeroesByOwner('DC'));
+//console.log(getHeroesByOwner('DC'));
+
+export {
+    getHeroeById,
+    getHeroesByOwner
+}
--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,14 +1,40 @@
-import heroes, {owners} from './data/heroes';
+import {getHeroeById} from './bases/09-Multiples-exportaciones-por-defecto';
+/*
+const promesa = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+        // console.log('2 segundos despues')
+        //resolve();
+        // Tarea
+        const heroe = getHeroeById(2);
+        //console.log(heroe);
+        //resolve(heroe)
+        reject('No se pudo encontrar el heroe');
+    }, 2000);
+});
 
-console.log(owners);
-
-const getHeroeById = (id) => {
-    return heroes.find((heroe) => heroe.id === id)
+promesa.then( (heroe) => {
+    //console.log("then de la promesa, se ejecuta el resolve")
+    console.log('heroe', heroe);
+})
+.catch( err => console.warn(err));
+*/
+const getHeroeByIdAsync = (id) =>{
+    const promesa = new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            // console.log('2 segundos despues')
+            //resolve();
+            // Tarea
+            const p1 = getHeroeById(id);
+            if(p1)
+                resolve(p1);
+            else    
+                reject('No se pudo encontrar el heroe');
+        }, 2000);
+    });
+    return promesa;
 }
-console.log(getHeroeById(2));
-
-//tarea
-const getHeroesByOwner = (owner) => {
-    return heroes.filter((heroe) => heroe.owner === owner)
-}
-console.log(getHeroesByOwner('DC'));
+getHeroeByIdAsync(10)
+    //.then( heroe => console.log('Heroe', heroe) )
+    //.catch( err => console.warn(err))
+    .then( console.log )
+    .catch( console.warn);
