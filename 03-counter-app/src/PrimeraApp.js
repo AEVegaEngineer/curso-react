24df9596c4cf504b1ed4f3ab94a26725a9df177c
Andres Vega <aevega1991@gmail.com>
2021-06-04 16:38:28 -0300

    37. Impresion de variables en el HTML

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -2,10 +2,17 @@
 import React from "react";
 
 const PrimeraApp = () => {
+    const saludo = 'Hola mundo const';
+    // const saludo = {
+    //     nombre: 'Fernando',
+    //     edad: 34
+    // }
     return ( 
     //<Fragment>
-    <>
-        <h1>Hola Mundo</h1>
+    // No se pueden imprimir objetos o booleanos de esta manera, si es necesario imprimir un objeto se debe hacer un JSON.stringify
+    <>      
+        <h1>{saludo}</h1>
+        {/* <pre>{JSON.stringify(saludo, null ,3)}</pre> */}
         <p>Mi primera aplicacion</p>
     </>
     //</Fragment>
