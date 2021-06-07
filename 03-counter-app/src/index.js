69e10b2482494812f2725f63a02e93de447e7fc4
Andres Vega <aevega1991@gmail.com>
2021-06-07 10:59:21 -0300

    41. Componente CounterApp

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeraApp from './PrimeraApp'
+//import PrimeraApp from './PrimeraApp'
+import CounterApp from './CounterApp'
 import './index.css';
 
 //const saludo = <h1>Hola Mundo</h1>;
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render(<PrimeraApp saludo='Hola soy Goku'/>, divRoot);
+// ReactDOM.render(<PrimeraApp saludo='Hola soy Goku'/>, divRoot);
+ReactDOM.render(<CounterApp value={123}/>, divRoot);
