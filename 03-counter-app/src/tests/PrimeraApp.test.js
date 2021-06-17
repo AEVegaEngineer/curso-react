14d9856698527db2604c3c3272f52226b0dc613e
Andres Vega <aevega1991@gmail.com>
2021-06-17 15:42:55 -0300

    59. Enzyme - Testing unit

--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,11 +1,22 @@
 
 import { render } from "@testing-library/react";
+import React from 'react'
+import { shallow } from 'enzyme'
 import PrimeraApp from '../PrimeraApp'
 
+
 describe('Pruebas en <PrimeraApp/>', () => {
+    /*
     test('Debe de mostrar el mensaje "Hola Soy Goku"', () => {
         const saludo = 'Hola, soy Goku';
         const {getByText} = render(<PrimeraApp saludo = {saludo}/>);
         expect( getByText(saludo) ).toBeInTheDocument();
     })    
+    */
+    test('debe de mostrar <PrimeraApp/> correctamente', () => {
+        const saludo = 'hola soy Goku';
+        const wrapper = shallow(<PrimeraApp saludo={saludo}/>) 
+        expect(wrapper).toMatchSnapshot();
+    })
+    
 })
