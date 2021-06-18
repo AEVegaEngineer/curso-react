7460948b26005ce9b72ddddb7ebaeefa758fa775
Andres Vega <aevega1991@gmail.com>
2021-06-18 17:07:44 -0300

    72. Comunicacion entre componentes

--- 04-gif-expert/src/GifExpertApp.js
+++ 04-gif-expert/src/GifExpertApp.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 const GifExpertApp = ( ) => {  
     
     const [categories, setCategories] = useState(['One Punch', 'Samuray X', 'Dragon Ball']);
-
+    /*
     const handleAdd = () => {   
         // se puede usar el operador spread para obtener los elementos que ya componen las categorias
         // y se agrega Hunter al final
@@ -11,11 +12,12 @@ const GifExpertApp = ( ) => {
         // lo agrega al principio
         // setCategories(['Hunter', ...categories]);
     }
+    */
     return (    
     <>      
         <h2>GifExpertApp</h2>
         <hr></hr>
-        <button onClick={handleAdd}>Agregar</button>
+        <AddCategory/>
         <ol>
             {
                 categories.map( category => {
