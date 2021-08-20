34deb5b5cbfa2e3e2232d80ff75bc3b97cae9a13
Andres Vega <aevega1991@gmail.com>
2021-08-20 19:37:30 -0300

    Fixeando bug para usar useCounter dentro de CounterWithCustomHooks

--- 05-hook-app/src/components/01-useState/CounterWithCustomHook.js
+++ 05-hook-app/src/components/01-useState/CounterWithCustomHook.js
@@ -3,17 +3,17 @@ import { useCounter } from '../../hooks/useCounter';
 import './counter.css';
 
 export const CounterWithCustomHook = () => {
-    const {state,increment,decrement,reset} = useCounter(100);
+    const {Counter,increment,decrement,reset} = useCounter(100);
     return (
         <>
-            <h1>Counter with Hook: {state} </h1>
+            <h1>Counter with Hook: {Counter} </h1>
             <hr></hr>
             {/* <button onClick={increment} className="btn btn-primary">+1</button> */}
             {/* No se puede colocar de este modo ya que como establecimos unos factores
             como parametros debemos tratarlo como funcion de flecha para que no tome
             el evento del clic como parametro (el evento que se envia por defecto) */}
-            <button onClick={() => increment(2)} className="btn btn-primary">+1</button>
-            <button onClick={() => decrement(2)} className="btn btn-danger">+2</button>
+            <button onClick={() => increment(2)} className="btn btn-primary">+2</button>
+            <button onClick={() => decrement(2)} className="btn btn-danger">-2</button>
             <button onClick={ reset } className="btn btn-danger">Reset</button>
         </>
     )
