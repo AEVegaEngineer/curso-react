8f566f92285f8b80fa287a449b84ebaa8b084065
Andres Vega <aevega1991@gmail.com>
2021-08-26 22:58:01 -0300

    153. Pruebas restantes de mi Reducer

--- 05-hook-app/src/tests/components/08-useReducer/todoReducer.test.js
+++ 05-hook-app/src/tests/components/08-useReducer/todoReducer.test.js
@@ -25,8 +25,29 @@ describe('Pruebas en todoReducer', () => {
       { id: 3, desc: 'Aprender Mongo', done: false }
     ]);
     
-  })
-  
-  
+  });
+
+  test('debe de borrar un TODO', () => {
+    // action.payload = id del todo
+    const action = {
+      type: 'delete',
+      payload: 1           
+    }
+    const state = todoReducer( demoTodos, action );
+    expect( state.length ).toBe(1);
+    const stateLeft = state.filter(todo => todo.id !== 1);
+    expect( state ).toEqual(stateLeft);
+  });
+
+  test('debe de hacer el toggle del TODO', () => {
+    const action = {
+      type: 'toggle',
+      payload: 2           
+    }
+    const state = todoReducer( demoTodos, action );
+    const [stateToggled] = state.filter(todo => todo.id === 2);
+    expect(stateToggled.done).toBe(true);
+    
+  });
 
 });
