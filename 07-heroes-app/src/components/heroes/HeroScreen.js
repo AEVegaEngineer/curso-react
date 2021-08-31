f169d804a272a283d3a7fa2c20cb9936e4b29387
Andres Vega <aevega1991@gmail.com>
2021-08-31 15:36:11 -0300

    177. Estilo del componente HeroScreen

--- 07-heroes-app/src/components/heroes/HeroScreen.js
+++ 07-heroes-app/src/components/heroes/HeroScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Redirect, useParams } from 'react-router-dom'
 import { getHeroById } from '../../selectors/getHeroById';
 
-export const HeroScreen = () => {
+export const HeroScreen = ({history}) => {
   const {heroeId} = useParams();
   const hero = getHeroById(heroeId);
   // como se va a desestructurar al heroe, se necesita revisar si el heroe no es undefined,
@@ -19,10 +19,42 @@ export const HeroScreen = () => {
     first_appearance,
     characters
   } = hero;
-  console.log(hero)
+
+  const handleReturn = () => {
+    // si se abre una ventana sin historial de navegacion directamente en un url de un heroe,
+    // ej. http://localhost:3000/hero/dc-batman, y luego se le da al boton de back, se sale
+    // la aplicacion, para evitarlo se puede hacer una validacion
+    if(history.length <= 2) {
+      history.push('/');
+    } else {
+      history.goBack();
+    }
+  }
   return (
-    <div>
-      <h1>Hero Screen</h1>
+    <div className='row mt-5'>
+      <div className='col-4'>
+        <img 
+          src={`../assets/heroes/${heroeId}.jpg`}
+          alt='superhero'
+          className='img-thumbnail'
+          />
+      </div>
+      <div className='col-8'>
+        <h3>{superhero}</h3>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'><b>Alter ego: {alter_ego}</b></li>
+          <li className='list-group-item'><b>Publisher: {publisher}</b></li>
+          <li className='list-group-item'><b>First appearance: {first_appearance}</b></li>
+        </ul>
+        <h5>Characters</h5>
+        <p>{characters}</p>
+        <button 
+          className='btn btn-outline-info'
+          onClick={handleReturn}
+        >
+          Return
+        </button>
+      </div>
     </div>
   )
 }
