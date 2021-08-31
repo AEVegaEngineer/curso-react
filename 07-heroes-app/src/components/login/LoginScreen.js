3048137d9dd35d38a1e31c817e6ef507bd294b39
Andres Vega <aevega1991@gmail.com>
2021-08-31 00:21:56 -0300

    173. History push / replace

--- 07-heroes-app/src/components/login/LoginScreen.js
+++ 07-heroes-app/src/components/login/LoginScreen.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
-export const LoginScreen = () => {
+/*
+Dentro del componente LoginScreen existen props predefinidas por el react router dom,
+para verlas, ir al inspector y en la pestana components clicear LoginScreen.
+Para acceder a uno de esos props se debe hacer desestructuracion en el componente, como
+ocurre abajo con el History
+*/
+export const LoginScreen = ({ history }) => {
+  const handleLogin = () => {
+    //history.push('/'); // redirecciona a la pagina de marvel
+    history.replace('/'); // reemplaza el historial de busqueda anterior (login) por la ruta '/' (MarvelScreen)
+  }
   return (
-    <div>
-      <h1>LoginScreen</h1>
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr/>
+      <button 
+      className="btn btn-primary"
+      onClick={handleLogin}
+      >
+        Ingresar
+      </button>
     </div>
   )
 }
