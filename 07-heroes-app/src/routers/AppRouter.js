afdfe3b8377fe50eaa07c8d7a906b5ab4de33c68
Andres Vega <aevega1991@gmail.com>
2021-09-02 18:51:53 -0300

    192. Rutas públicas

--- 07-heroes-app/src/routers/AppRouter.js
+++ 07-heroes-app/src/routers/AppRouter.js
@@ -1,13 +1,13 @@
 import React, { useContext } from "react";
 import {
   BrowserRouter as Router,
-  Switch,
-  Route
+  Switch
 } from "react-router-dom";
 import { AuthContext } from "../auth/AuthContext";
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
 import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   const {user} = useContext(AuthContext);
@@ -16,7 +16,12 @@ export const AppRouter = () => {
     <Router>
       <div className="container mt-2">
         <Switch>
-          <Route exact path="/login" component={LoginScreen}/>
+          <PublicRoute 
+            exact
+            path="/login" 
+            component={LoginScreen}
+            isAuthenticated={user.logged}
+          />
           <PrivateRoute             
             path="/" 
             component={DashboardRoutes}
