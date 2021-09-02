6f34503d99b0bd3d24e25f1b48c0c3c075cfcb59
Andres Vega <aevega1991@gmail.com>
2021-09-02 17:16:33 -0300

    189. Login de un usuario

--- 07-heroes-app/src/components/login/LoginScreen.js
+++ 07-heroes-app/src/components/login/LoginScreen.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 /*
 Dentro del componente LoginScreen existen props predefinidas por el react router dom,
@@ -7,9 +9,19 @@ Para acceder a uno de esos props se debe hacer desestructuracion en el component
 ocurre abajo con el History
 */
 export const LoginScreen = ({ history }) => {
+  
+  const {dispatch} = useContext(AuthContext);
   const handleLogin = () => {
     //history.push('/'); // redirecciona a la pagina de marvel
-    history.replace('/'); // reemplaza el historial de busqueda anterior (login) por la ruta '/' (MarvelScreen)
+    //(MarvelScreen)    
+    const actionLogin = {
+      type: types.login,
+      payload: {
+        name:'Andres'
+      }
+    }
+    dispatch(actionLogin);
+    history.replace('/'); // reemplaza el historial de busqueda anterior (login) por la ruta '/' 
   }
   return (
     <div className="container mt-5">
