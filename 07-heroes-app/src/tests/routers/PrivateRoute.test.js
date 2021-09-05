8e962b91d3ef95fd840d878756f8969987727bc4
Andres Vega <aevega1991@gmail.com>
2021-09-05 00:46:30 -0300

    199. Pruebas en el PrivateRoute

---
+++ 07-heroes-app/src/tests/routers/PrivateRoute.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { PrivateRoute } from '../../routers/PrivateRoute';
+import { MemoryRouter } from 'react-router';
+
+describe('Pruebas en <PrivateRoute/>',() => {
+  const rest = {
+    location: {
+      pathname: '/marvel'
+    }
+  }
+  test('debe de mostrar el componente si esta autenticado y guardar localStorage', () => {
+    // cuando se tiene el tipo de estructura que tiene un higher order component, no se puede
+    // usar shallow, ya que solo va a renderizar, en este caso, al MemoryRouter
+    const wrapper = mount(
+      // el memory router es un higher order component hecho para hacer pruebas de router con ciertas rutas
+      // por ejemplo PrivateRoute debe estar dentro de un router
+      <MemoryRouter>
+        <PrivateRoute   
+          isAuthenticated={true}
+          component={() => <span>Listo</span>}
+          {...rest}
+        />
+      </MemoryRouter>
+    )
+    expect(wrapper.find('span').exists()).toBe(true);
+  });
+  test('NO debe de mostrar el componente si NO esta autenticado', () => {
+    const wrapper = mount(
+      <MemoryRouter>
+        <PrivateRoute   
+          isAuthenticated={false}
+          component={() => <span>Listo</span>}
+          {...rest}
+        />
+      </MemoryRouter>
+    )
+    expect(wrapper.find('span').exists()).toBe(false);
+  });
+  
+  
+});
