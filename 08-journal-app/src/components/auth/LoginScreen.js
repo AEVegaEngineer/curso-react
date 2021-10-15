5a24658a4e4e54b0d755c03b3aec7c67f71dea2f
Andres Vega <aevega1991@gmail.com>
2021-10-15 15:33:31 -0300

    222. Estilos del LoginScreen

--- 08-journal-app/src/components/auth/LoginScreen.js
+++ 08-journal-app/src/components/auth/LoginScreen.js
@@ -1,15 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const LoginScreen = () => {
   return (
     <>
-      <h3>Login</h3>
+      <h3 className="auth__tittle">Login</h3>
       <form>
-        <input type="text" placeholder="email" name="email"/>
-        <input type="password" placeholder="password" name="password"/>
-        <button type="submit" name="btnLogin">Login</button>
+        <input type="text" placeholder="Email" name="email" className="auth__input"/>
+        <input type="password" placeholder="Password" name="password" autoComplete="off" className="auth__input" />
+        <button type="submit" name="btnLogin" className='btn btn-primary btn-block'>Login</button>
         <hr></hr>
-        google
+        <div className="auth__social-network">
+          <p>Login with social media</p>
+          <div 
+              className="google-btn"
+          >
+              <div className="google-icon-wrapper">
+                  <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+              </div>
+              <p className="btn-text">
+                  <b>Sign in with google</b>
+              </p>
+          </div>
+          <Link to="/auth/register">Create new account</Link>
+        </div>
       </form>
     </>
   )
