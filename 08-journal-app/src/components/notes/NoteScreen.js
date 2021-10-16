134e00c2e25aa025d990168a6a9ac671bb195cbc
Andres Vega <aevega1991@gmail.com>
2021-10-16 15:32:28 -0300

    228. Estructura y diseño del HomeScreen

--- 08-journal-app/src/components/notes/NoteScreen.js
+++ 08-journal-app/src/components/notes/NoteScreen.js
@@ -5,6 +5,24 @@ export const NoteScreen = () => {
   return (
     <div className="notes__main-content">
       <NotesAppBar/>
+
+      <div className="notes__content">
+        <input
+          type="text"
+          placeholder="Some awesome title"
+          className="notes__title-input"
+        />
+        <textarea
+          placeholder="What happend today?"
+          className="notes__textarea"
+        ></textarea>
+
+        <div className="notes__image">
+          <img alt="Cielo" src="https://play-lh.googleusercontent.com/3-AOeSSoqN3IK750gMz4FwOJw0MnIT-_dSA2Ujs9MzcS5bMaV9bwTBwH2udoEmBsBKY"/>
+        </div>
+
+        
+      </div>
     </div>
   )
 }
