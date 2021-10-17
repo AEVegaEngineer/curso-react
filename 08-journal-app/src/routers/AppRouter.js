91946892e3c18a9b2fd541a66b440eaa80fa5724
Andres Vega <aevega1991@gmail.com>
2021-10-17 20:05:12 -0300

    249. Mantener el estado de la autenticación al recargar

--- 08-journal-app/src/routers/AppRouter.js
+++ 08-journal-app/src/routers/AppRouter.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import { getAuth } from '@firebase/auth'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
+import { login } from '../actions/auth'
 import { JournalScreen } from '../components/journal/JournalScreen'
 import { AuthRouter } from './AuthRouter'
 
 export const AppRouter = () => {
+
+  // Mantener el estado de la autenticacion de firebase al recargar con un observable
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getAuth().onAuthStateChanged((user) => {
+      if(user?.uid){
+        dispatch(login(user.uid, user.displayName));
+      }
+    });
+  }, [dispatch])
+
   return (    
     <Router> 
       <div>
