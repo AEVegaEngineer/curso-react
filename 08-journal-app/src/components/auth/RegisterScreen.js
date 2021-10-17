9e6f3eba011479b487089148e7f13f04929d9fb5
Andres Vega <aevega1991@gmail.com>
2021-10-17 12:53:19 -0300

    243. Manejo de errores del formulario

--- 08-journal-app/src/components/auth/RegisterScreen.js
+++ 08-journal-app/src/components/auth/RegisterScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 //import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom'
 import { useForm } from '../../hooks/useForm';
+import validator from 'validator'
 
 export const RegisterScreen = () => {
 
@@ -18,7 +19,9 @@ export const RegisterScreen = () => {
 
   const handleRegister = (e) => {
     e.preventDefault();
-    console.log(name ,email, password, confirm);
+    if(isFormValid()){      
+      console.log(name ,email, password, confirm);
+    }
     //dispatch(startRegisterEmailPassword(email, password));
   }
   
@@ -27,13 +30,35 @@ export const RegisterScreen = () => {
   // }
 
   const isFormValid = () => {
-
+    let errorOcurred = false;
+    if(name.trim().length === 0){
+      console.log('name is required'); 
+      errorOcurred = true;     
+    }
+    if(!validator.isEmail(email)){
+      console.log('email is not valid');
+      errorOcurred = true;
+    }
+    if(password !== confirm){
+      console.log('passwords do not match');
+      errorOcurred = true;
+    }
+    if(password.length < 6){
+      console.log('password length must be at least 6 characters');
+      errorOcurred = true;
+    }
+    return !errorOcurred;
   }
 
   return (
     <>
       <h3 className="auth__tittle">Register</h3>
       <form onSubmit={handleRegister}>
+
+        <div className="auth__alert-error">
+          Ha ocurrido un error
+        </div>
+
         <input type="text" placeholder="Name" name="name" 
           className="auth__input" 
           value={name}
