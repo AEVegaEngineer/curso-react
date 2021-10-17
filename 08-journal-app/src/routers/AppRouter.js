6d5618bfad793dc64aeca8b2c42ee50e353d2e17
Andres Vega <aevega1991@gmail.com>
2021-10-17 20:19:59 -0300

    250. Mostrar un loading global en la aplicación

--- 08-journal-app/src/routers/AppRouter.js
+++ 08-journal-app/src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import { getAuth } from '@firebase/auth'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
 import { login } from '../actions/auth'
@@ -11,14 +11,34 @@ export const AppRouter = () => {
   // Mantener el estado de la autenticacion de firebase al recargar con un observable
 
   const dispatch = useDispatch();
+  
+  // checking  es un estado simple, la idea es mostrar un texto
+  // de precarga de pantalla cuando se presiona F5, inicialmente
+  // el valor de checking es siempre true, una vez se obtiene 
+  // una respuesta del efecto, se setea checking en false y se
+  // muestra la pagina.
+  const [checking, setChecking] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
 
   useEffect(() => {
     getAuth().onAuthStateChanged((user) => {
       if(user?.uid){
         dispatch(login(user.uid, user.displayName));
+        setIsLoggedIn(true);
+      } else { 
+        setIsLoggedIn(false);
       }
+      setChecking(false);
     });
-  }, [dispatch])
+  }, [dispatch,setChecking])
+
+  
+  if(checking) {
+    return (
+      <h1>Espere...</h1>
+    )
+  }
 
   return (    
     <Router> 
