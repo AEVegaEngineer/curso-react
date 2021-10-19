aa3ad1245b4fab58e8df598c72b346363cf5d27d
Andres Vega <aevega1991@gmail.com>
2021-10-19 14:47:03 -0300

    265. Activar una nota para su edición

--- 08-journal-app/src/components/notes/NoteScreen.js
+++ 08-journal-app/src/components/notes/NoteScreen.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { NotesAppBar } from './NotesAppBar'
+import { useForm } from '../../hooks/useForm'
 
 export const NoteScreen = () => {
+
+  //active:note renombra a note la desestructuracion de la nota activa
+  const {active:note} = useSelector(state => state.notes); 
+  
+  const [ formValues, handleInputChange ] = useForm( note );
+
+  const {body, title} = formValues;
+
   return (
     <div className="notes__main-content">
       <NotesAppBar/>
@@ -11,17 +21,22 @@ export const NoteScreen = () => {
           type="text"
           placeholder="Some awesome title"
           className="notes__title-input"
+          value={title}
+          onChange={handleInputChange}
         />
         <textarea
           placeholder="What happend today?"
           className="notes__textarea"
-        ></textarea>
-
-        <div className="notes__image">
-          <img alt="Cielo" src="https://play-lh.googleusercontent.com/3-AOeSSoqN3IK750gMz4FwOJw0MnIT-_dSA2Ujs9MzcS5bMaV9bwTBwH2udoEmBsBKY"/>
-        </div>
-
-        
+          onChange={handleInputChange}
+        >
+          {body}
+        </textarea>
+        {
+          (note.url) &&
+          <div className="notes__image">
+            <img alt="Cielo" src="https://play-lh.googleusercontent.com/3-AOeSSoqN3IK750gMz4FwOJw0MnIT-_dSA2Ujs9MzcS5bMaV9bwTBwH2udoEmBsBKY"/>
+          </div>
+        }
       </div>
     </div>
   )
