550e40199b13c8ba15de70cdcaace12d07ce5547
Andres Vega <aevega1991@gmail.com>
2021-10-19 14:04:58 -0300

    264. Mostrar las notas en el menú lateral

--- 08-journal-app/src/components/journal/JournalEntry.js
+++ 08-journal-app/src/components/journal/JournalEntry.js
@@ -1,28 +1,33 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
 
-export const JournalEntry = () => {
+export const JournalEntry = ({id, date, title, body, url}) => {
+  const noteDate = moment(date);
   return (
     <div className="journal__entry pointer">
-      <div 
-      className="journal__entry-picture"
-      style={{
-        backgroundSize: 'cover',
-        backgroundImage: 'url(https://lh3.googleusercontent.com/proxy/svZ03aUpWLt762phkNPQM-ejHtBFFJu0zp3bYyc_ByJiYkKbjxvnkFPQodP7zkm7UbuaD_gMA49jPBFtrXjlaZ2-oHqIrk6felSMKYC_sm0SL6Nf2BKWwt_eJIbx0FMV8wdEs2gEKqzjNQwdww26zlIoUBI-5SSN6IX6nnizmqbyRLsDBnuvcQ)'
-      }}
-      ></div>
+      {
+        url &&
+        <div 
+          className="journal__entry-picture"
+          style={{
+            backgroundSize: 'cover',
+            backgroundImage: `url(${url})`
+          }}
+        ></div>
+      }
 
       <div className="journal__entry-body">
         <p className="journal__entry-title">
-          Un nuevo dia
+          {title}
         </p>
         <p className="journal__entry-content">
-          duashd hdash dsjhd sjhd sjh dajhdasjh as asdasdas asdasdas.
+          {body}
         </p>
       </div>
 
       <div className="journal__entry-date-box">
-        <span>Monday</span>
-        <h4>28</h4>
+        <span>{noteDate.format('dddd')}</span>
+        <h4>{noteDate.format('Do')}</h4>
       </div>
     </div>
   )
