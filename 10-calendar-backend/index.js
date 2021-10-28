7aec87c6636042a8ab102818d4cffd0e356c09e0
Andres Vega <aevega1991@gmail.com>
2021-10-28 13:36:01 -0300

    324. Variables de entorno y carpeta pública

--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -1,1 +1,23 @@
-console.log("Hola mundo");
+const express = require('express');
+const dotenv = require('dotenv')
+dotenv.config();
+console.log(process.env);
+
+const app = express();
+
+// Directorio publico
+app.use( express.static('public') );
+
+
+// Rutas
+app.get('/', (req, res) => {
+  console.log('se requiere el /');
+  res.json({
+    ok: true
+  })
+});
+
+//Escuchar peticiones
+app.listen( process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+} );
