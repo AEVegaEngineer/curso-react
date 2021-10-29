0caea9efcb702ba53aa944051e0a4d1cca2a008f
Andres Vega <aevega1991@gmail.com>
2021-10-29 15:00:31 -0300

    333. Validaciones del usuario

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -5,13 +5,26 @@ const Usuario = require('../models/Usuario');
 
 const crearUsuario = async(req, res = response) => {  
   //console.log(req.body)
-  //const {name,email,password} = req.body;
+  
+  const {name,email,password} = req.body;
   try {
-    const usuario = new Usuario(req.body);
+    // OJO, esto debe tener await, sino siempre devuelve la misma response
+    // y es el equivalente a que cualquier usuario ya se encuentre registrado
+    let usuario = await Usuario.findOne({email: email});
+    // console.log('*************************************')
+    // console.log(usuario)
+    if( usuario ){
+      return res.status(400).json({
+        ok:false,
+        msg: 'Ya existe un usuario con el mismo correo electrónico'
+      })
+    }
+    usuario = new Usuario(req.body);
     await usuario.save();
     return res.status(201).json({
       ok: true, 
-      msg: 'registro'
+      uid: usuario.id, 
+      name: usuario.name
     });  
     
   } catch (error) {
