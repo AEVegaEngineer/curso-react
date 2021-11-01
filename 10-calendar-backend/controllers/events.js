7f65947ec122a82eb33035bf2a9ca53f49450923
Andres Vega <aevega1991@gmail.com>
2021-11-01 18:04:24 -0300

    349. Eliminar Eventos

--- 10-calendar-backend/controllers/events.js
+++ 10-calendar-backend/controllers/events.js
@@ -16,16 +16,17 @@ const crearEvento = async(req, res = response) => {
   try {
     evento.user = req.uid;
     const eventoGuardado = await evento.save();
-    res.json({ 
-      ok: true,
-      evento: eventoGuardado
-    });    
     console.log('********* Guardado nuevo evento. '+longDateFormat(Date.now())+' *********');
     console.log(eventoGuardado)
+    return res.json({ 
+      ok: true,
+      evento: eventoGuardado,
+      msg: 'Evento guardado'
+    });    
   } catch (error) {
     console.log('********* Error en crearEvento. '+longDateFormat(Date.now())+' *********');
     console.log(error);
-    res.status(500).json({
+    return res.status(500).json({
       ok: false,
       msg: 'Hable con el administrador'
     })
@@ -38,16 +39,16 @@ const actualizarEvento = async(req, res = response) => {
   try {
     const evento = await Evento.findById(eventoId);
     if(!evento) {
-      res.status(404).json({
+      console.log('********* Error en actualizarEvento. Evento no encontrado. '+longDateFormat(Date.now())+' *********');
+      console.log('eventoId:'+eventoId);
+      return res.status(404).json({
         ok: false,
         msg: 'No existe un evento con ese id'
       });
-      console.log('********* Error en actualizarEvento. Evento no encontrado. '+longDateFormat(Date.now())+' *********');
-      console.log('eventoId:'+eventoId);
     }
     //NO PERMITIR QUE UN USUARIO EDITE EL EVENTO DE OTRO USUARIO
     if( evento.user.toString() !== uid ){
-      res.status(401).json({
+      return res.status(401).json({
         ok: false,
         msg: 'No tiene privilegio para editar este evento'
       });
@@ -61,16 +62,17 @@ const actualizarEvento = async(req, res = response) => {
     // si se necesita que retorne el objeto nuevo, es necesario pasar
     // opciones en el tercer argumento del update
     const eventoActualizado = await Evento.findByIdAndUpdate( eventoId, nuevoEvento, { new: true } );
-    res.json({
-      ok: true,
-      evento: eventoActualizado
-    }); 
     console.log('********* Evento actualizado. '+longDateFormat(Date.now())+' *********');
     console.log({...req.body, eventoId});
+    return res.json({
+      ok: true,
+      evento: eventoActualizado,
+      msg: 'Evento actualizado',
+    }); 
   } catch (error) {
     console.log('********* Error en actualizarEvento. '+longDateFormat(Date.now())+' *********');
     console.log(error);
-    res.status(500).json({
+    return res.status(500).json({
       ok: false,
       msg: 'Hable con el administrador'
     })
@@ -78,10 +80,43 @@ const actualizarEvento = async(req, res = response) => {
 }
 
 const eliminarEvento = async(req, res = response) => {  
-  return res.status(200).json({
-    ok: true,
-    msg: 'eliminarEvento'
-  }); 
+  const eventoId = req.params.id;
+  const uid = req.uid;
+  try {
+    const evento = await Evento.findById(eventoId);
+    if(!evento) {
+      console.log('********* Error en eliminarEvento. Evento no encontrado. '+longDateFormat(Date.now())+' *********');
+      console.log('eventoId:'+eventoId);
+      return res.status(404).json({
+        ok: false,
+        msg: 'No existe un evento con ese id'
+      });
+    }
+    //NO PERMITIR QUE UN USUARIO EDITE EL EVENTO DE OTRO USUARIO
+    if( evento.user.toString() !== uid ){
+      res.status(401).json({
+        ok: false,
+        msg: 'No tiene privilegio para eliminar este evento'
+      });
+    }    
+    
+    const eventoEliminado = await Evento.findByIdAndDelete(eventoId);
+    console.log('********* Evento eliminado. '+longDateFormat(Date.now())+' *********');
+    console.log({...req.body, eventoId});
+    return res.json({
+      ok: true,
+      msg: 'Evento eliminado',
+      evento: eventoEliminado
+    }); 
+    
+  } catch (error) {
+    console.log('********* Error en eliminarEvento. '+longDateFormat(Date.now())+' *********');
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador'
+    })
+  }
 }
 
 module.exports = {
