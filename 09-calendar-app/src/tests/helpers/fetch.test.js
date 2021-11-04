64bdfa8403483e2d1ad6af26754226563a786520
Andres Vega <aevega1991@gmail.com>
2021-11-04 13:47:57 -0300

    384. Pruebas con el helper FechConToken

--- 09-calendar-app/src/tests/helpers/fetch.test.js
+++ 09-calendar-app/src/tests/helpers/fetch.test.js
@@ -1,11 +1,21 @@
-import { fetchSinToken } from "../../helpers/fetch";
+import { fetchConToken, fetchSinToken } from "../../helpers/fetch";
 
 describe('Pruebas en el helper de Fetch', () => {
+  let token = '';
   test('fetchSinToken debe funcionar', async() => {
     const resp = await fetchSinToken('auth', {email: 'aevega1991@gmail.com', password: '123456'}, 'POST');
     expect( resp instanceof Response).toBe(true);
 
     const body = await resp.json();
     expect( body.ok ).toBe(true);
+    token = body.token;
+  });
+
+  test('fetchConToken debe funcionar', async() => {
+    // valida que haya guardado un token en el localstorage y lo utiliza para la peticion
+    localStorage.setItem('token',token);
+    const resp = await fetchConToken('events/618053b4331741ffe12d4b01',{},'DELETE');
+    const body = await resp.json();
+    expect(body.msg).toBe('No existe un evento con ese id');
   });
 });
