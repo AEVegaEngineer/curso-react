1866d6a05a9fd4b2b07a546869691803606f9415
Andres Vega <aevega1991@gmail.com>
2021-11-04 14:10:05 -0300

    386. Pruebas con un login incorrecto

--- 09-calendar-app/src/tests/actions/auth.test.js
+++ 09-calendar-app/src/tests/actions/auth.test.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 import '@testing-library/jest-dom';
 import { startLogin } from '../../actions/auth';
 import { types } from '../../types/types';
+import Swal from 'sweetalert2';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -36,5 +37,18 @@ describe('Pruebas en las acciones Auth', () => {
     // calls es un array, si se hace console.log se pueden ver los argumentos
     //const token = localStorage.setItem.mock.calls[0][1];
     //console.log(token)
+  });
+
+  test('startLogin incorrecto', async() => {
+    // contraseña incorrecta
+    await store.dispatch( startLogin('fernando@gmail.com', '1234562'));
+    let actions = store.getActions();
+    expect(actions).toEqual([]);
+    expect( Swal.fire ).toHaveBeenCalledWith("Error", "Usuario o contraseña incorrecta", "error");
+
+    // email incorrecto
+    await store.dispatch( startLogin('fernando2@gmail.com', '123456'));
+    actions = store.getActions();
+    expect( Swal.fire ).toHaveBeenCalledWith("Error", "Usuario o contraseña incorrecta", "error");
   });
 })
