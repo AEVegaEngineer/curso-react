4e59c9e30ae38593787d616fa335f1db6c28b9c1
Andres Vega <aevega1991@gmail.com>
2021-11-05 13:30:11 -0300

    396. Pruebas cuando las contraseñas no son iguales

--- 09-calendar-app/src/tests/components/ui/auth/AuthScreen.test.js
+++ 09-calendar-app/src/tests/components/ui/auth/AuthScreen.test.js
@@ -6,11 +6,13 @@ import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import '@testing-library/jest-dom';
 import { AuthScreen } from '../../../../components/auth/AuthScreen';
-import { startLogin } from '../../../../actions/auth';
+import { startLogin, startRegister } from '../../../../actions/auth';
+import Swal from 'sweetalert2';
 
 
 jest.mock('../../../../actions/auth',() => ({
-  startLogin: jest.fn()
+  startLogin: jest.fn(),
+  startRegister: jest.fn()
 }))
 
 const middlewares = [thunk];
@@ -26,6 +28,13 @@ const wrapper = mount(
 );
 
 describe('Pruebas en <AuthScreen/>', () => {
+  // siempre que se usen mocks se deben limpiar luego de cada prueba
+  // sino acarreara datos de las distintas llamadas entre pruebas
+  beforeEach( () => {
+    jest.clearAllMocks();
+  })
+
+
   test('Debe mostrarse correctamente', () => {
     expect(wrapper).toMatchSnapshot();
   });
@@ -43,9 +52,73 @@ describe('Pruebas en <AuthScreen/>', () => {
         value: '123456'
       }
     });
+    // dispara el primer formulario (login, osea at(0)) 
     wrapper.find('form').at(0).prop('onSubmit')({
       preventDefault(){}
     });
     expect(startLogin).toHaveBeenCalledWith('juan@gmail.com','123456');
+  });
+
+  test('No hay registro si las contrasenas son diferentes', () => {
+
+    wrapper.find('input[name="rEmail"]').simulate('change', {
+      target: {
+        name: 'rEmail',
+        value: 'juan@gmail.com'
+      }
+    });
+    wrapper.find('input[name="rPassword1"]').simulate('change', {
+      target: {
+        name: 'rPassword1',
+        value: '123456'
+      }
+    });
+    wrapper.find('input[name="rPassword2"]').simulate('change', {
+      target: {
+        name: 'rPassword2',
+        value: '1234567'
+      }
+    });
+    // dispara el segundo formulario (registro, osea at(1)) 
+    wrapper.find('form').at(1).prop('onSubmit')({
+      preventDefault(){}
+    });
+    expect(startRegister).not.toHaveBeenCalled();
+    expect( Swal.fire ).toHaveBeenCalledWith("Error",'Las contraseñas deben de ser iguales', "error");
+  });
+
+  test('Registrar si las contraseñas son iguales', () => {
+    wrapper.find('input[name="rName"]').simulate('change', {
+      target: {
+        name: 'rName',
+        value: 'juan'
+      }
+    });
+    wrapper.find('input[name="rEmail"]').simulate('change', {
+      target: {
+        name: 'rEmail',
+        value: 'juan@gmail.com'
+      }
+    });
+    wrapper.find('input[name="rPassword1"]').simulate('change', {
+      target: {
+        name: 'rPassword1',
+        value: '123456'
+      }
+    });
+    wrapper.find('input[name="rPassword2"]').simulate('change', {
+      target: {
+        name: 'rPassword2',
+        value: '123456'
+      }
+    });
+    // dispara el segundo formulario (registro, osea at(1)) 
+    wrapper.find('form').at(1).prop('onSubmit')({
+      preventDefault(){}
+    });
+    expect(startRegister).toHaveBeenCalledWith('juan@gmail.com','123456','juan');   
+    // este expect requiere que se limpien los mocks, sino, como ya se habia llamado anteriormente
+    // el metodo fire, el mock retiene esa llamada y la muestra en esta prueba 
+    expect( Swal.fire ).not.toHaveBeenCalled();
   });
 });
